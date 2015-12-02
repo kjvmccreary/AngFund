f0f5c489707ee66dbd83a0c11ecb17e1d7895cb1
kmccreary <kevin@smartgroup-us.com>
2015-12-02 07:00:37 -0600

    Through Angular Controllers & Markup | Demo Handling Events

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -18,15 +18,39 @@ eventsApp.controller('EventController',
             imageUrl: '/img/angularjs-logo.png',
             sessions: [
                 {
-                    name: 'Directives Masterclass'
+                    name: 'Directives Masterclass',
+                    creatorName: "Bob Smith",
+                    duration: '1 hr',
+                    level: 'Advanced',
+                    abstract: 'In this session you will learn the ins and outs of directives in Angular.',
+                    upVoteCount: 0
                 },
                 {
-                    name: 'Scopes for fun and profit'
+                    name: 'Scopes for fun and profit',
+                    creatorName: "John Doe",
+                    duration: '30 min',
+                    level: 'Introductory',
+                    abstract: 'This session will take a close look at scopes..',
+                    upVoteCount: 0
                 },
                 {
-                    name: 'Well behaved Controllers'
+                    name: 'Well behaved Controllers',
+                    creatorName: "Jane Doe",
+                    duration: '2 hrs',
+                    level: 'Intermediate',
+                    abstract: 'Controllers are the beginning of everything in Angular.',
+                    upVoteCount: 0
                 }
             ]
+        }
+
+        $scope.upVoteSession = function(session) {
+            session.upVoteCount ++;
+        }
+        $scope.downVoteSession = function(session) {
+            if (session.upVoteCount > 0) {
+                session.upVoteCount --;
+            }
         }
     }
 
